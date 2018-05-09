a4dec4a90188e98076f2af41038476ac9969cce3
Oleksii Zamekula <zamekula.oleksii@gmail.com>
2018-05-09 17:37:37 +0300

    Starting core developing. Added class Parking which implements pattern "Singletone"

---
+++ Parking/Parking/Core/Parking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.Core
+{
+    public sealed class Parking
+    {
+        private static readonly Lazy<Parking> lazy = new Lazy<Parking>(() => new Parking());
+        public static Parking Instanse { get { return lazy.Value; } }
+
+        private Parking()
+        {
+
+        }
+    }
+}
---
+++ Parking/Parking/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Parking.Core;
+
+namespace Parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Core.Parking p = Core.Parking.Instanse;
+        }
+    }
+}
