78d49382c1649144aba2e70e211e531333f96f2c
Oleksii Zamekula <zamekula.oleksii@gmail.com>
2018-05-09 18:36:33 +0300

    Add fields and properties to Parking class.

--- Parking/Parking/Core/Parking.cs
+++ Parking/Parking/Core/Parking.cs
@@ -11,8 +11,15 @@ namespace Parking.Core
         private static readonly Lazy<Parking> lazy = new Lazy<Parking>(() => new Parking());
         public static Parking Instanse { get { return lazy.Value; } }
 
+        private readonly Settings settings;
+
+        List<Car> cars;
+        List<Transaction> transactions;
+        public double Balance { get; private set; }
+
         private Parking()
         {
+            settings = new Settings();
 
         }
     }
