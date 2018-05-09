c3c77e0e52b7ec5226e22cce2aee2c2de057e992
Oleksii Zamekula <zamekula.oleksii@gmail.com>
2018-05-09 18:28:39 +0300

    Transaction calss was added

---
+++ Parking/Parking/Core/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.Core
+{
+    public class Transaction
+    {
+        public readonly DateTime transacionTime;
+        public DateTime TransactionTime { get { return transacionTime; } }
+        public string GUID { get; protected set; }
+        public double Tax { get; protected set; }
+
+    }
+}
