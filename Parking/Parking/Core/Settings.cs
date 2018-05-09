c2c014c28e7ca3fe967440a80eb8053521be8468
Oleksii Zamekula <zamekula.oleksii@gmail.com>
2018-05-09 18:32:41 +0300

    Added Settings class

---
+++ Parking/Parking/Core/Settings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.Core
+{
+    class Settings
+    {
+        public int Timeout { get; set; }
+        public Dictionary<string,double> TaxesForCarType { get; set; }
+        public int ParkingSpace { get; set; }
+        public double Fine { get; set; }
+    }
+}
